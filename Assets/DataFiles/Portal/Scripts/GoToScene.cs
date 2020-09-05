614b3ba5e4578dcc4f392191a7d3361be3e2e59d
tirth-hs <tirthshah200@gmail.com>
2020-09-05 15:56:26 +0530

    Added portal home scene

---
+++ Assets/DataFiles/Portal/Scripts/GoToScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class GoToScene : MonoBehaviour
+{
+    public void backbtn()
+    {
+        SceneManager.LoadScene("ExplorePlanets");
+    }
+    public void beach()
+    {
+        SceneManager.LoadScene("BeachScene");
+    }
+    public void underwater()
+    {
+        SceneManager.LoadScene("UnderwaterScene");
+    }
+    public void northenlights()
+    {
+        SceneManager.LoadScene("NorthernLights");
+    }
+
+}
